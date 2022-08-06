f09ce6c6e1467517a1f131e6ba6b680ca7df2b0a
firstpersoncode <fpcdatadriver@gmail.com>
2022-08-06 11:25:38 +0800

    swipeable screens

--- components/Screen/Home/index.jsx
+++ components/Screen/Home/index.jsx
@@ -1,7 +1,3 @@
-import { useAppSessionContext } from "context/AppSession";
-
 export default function Home() {
-  const user = useAppSessionContext();
-
-  return <pre>{JSON.stringify(user, null, 4)}</pre>;
+  return "Home";
 }
--- components/Screen/index.jsx
+++ components/Screen/index.jsx
@@ -1,22 +1,55 @@
 import { useRouter } from "next/router";
+import SwipeableViews from "react-swipeable-views";
 import AppLayout from "../AppLayout";
 
-const componentPages = {
-  "/": require("./Home").default,
-  "/school": require("./School").default,
-  "/participant": require("./Participant").default,
-  "/setting": require("./Setting").default,
-};
+import Home from "./Home";
+import School from "./School";
+import Participant from "./Participant";
+import Setting from "./Setting";
+import { Box } from "@mui/material";
+
+// const componentPages = {
+//   "/": require("./Home").default,
+//   "/school": require("./School").default,
+//   "/participant": require("./Participant").default,
+//   "/setting": require("./Setting").default,
+// };
 
 export default function Page() {
-  const { asPath } = useRouter();
+  const { asPath, push } = useRouter();
+  const paths = ["/", "/school", "/participant", "/setting"];
 
-  const ComponentPage = componentPages[asPath];
-  if (!ComponentPage) return null;
+  const handleChangeIndex = (index) => {
+    push(paths[index]);
+  };
+
+  // const ComponentPage = componentPages[asPath];
+  // if (!ComponentPage) return null;
 
   return (
     <AppLayout>
-      <ComponentPage />
+      {/* <ComponentPage /> */}
+
+      <SwipeableViews
+        index={paths.findIndex((p) => p === asPath)}
+        onChangeIndex={handleChangeIndex}
+      >
+        <Box sx={{ minHeight: "70vh" }}>
+          <Home />
+        </Box>
+
+        <Box sx={{ minHeight: "70vh" }}>
+          <School />
+        </Box>
+
+        <Box sx={{ minHeight: "70vh" }}>
+          <Participant />
+        </Box>
+
+        <Box sx={{ minHeight: "70vh" }}>
+          <Setting />
+        </Box>
+      </SwipeableViews>
     </AppLayout>
   );
 }
