6abd496770d74ec3e5cbebc877641977cafe5f15
firstpersoncode <fpcdatadriver@gmail.com>
2022-08-09 22:45:01 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = () => {
   return {
-    // reactStrictMode: true,
-    // swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
     env: {
       BUILD_ENV: process.env.BUILD_ENV,
-      DASHBOARD_URL: process.env.DASHBOARD_URL,
-      DASHBOARD_API_KEY: process.env.DASHBOARD_API_KEY,
+      MONGODB_URI: process.env.MONGODB_URI,
+
+      SESSION_NAME: process.env.SESSION_NAME,
+      SESSION_ROLE: process.env.SESSION_ROLE,
       SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY,
+      API_KEY: process.env.API_KEY,
 
       HCAPTCHA_SITE_KEY: process.env.HCAPTCHA_SITE_KEY,
       HCAPTCHA_SECRET_KEY: process.env.HCAPTCHA_SECRET_KEY,
